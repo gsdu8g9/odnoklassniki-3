78816dd9ca5aa13846c3cdb33c8bbbb7ccf72e3b
kingdomik <dnsk@mail.ru>
2014-05-21 21:49:21 +0400

    comments

--- src/ru/odnoklassniki/tests/ui/api/selenium/DefaultSelenium2.java
+++ src/ru/odnoklassniki/tests/ui/api/selenium/DefaultSelenium2.java
@@ -23,7 +23,7 @@ public class DefaultSelenium2 extends DefaultSelenium implements Selenium2 {
 			String browserStartCommand, String browserURL) {
 		super(serverHost, serverPort, browserStartCommand, browserURL);
 		
-		// Wrap commandProcessor to log all interaction with Selenium RC locally
+		// Wrap commandProcessor to log all interaction with Selenium RC on client side
 		commandProcessor = new LogggedCommandProcessor(commandProcessor);
 	}
 
