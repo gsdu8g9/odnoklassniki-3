ebd9d28bc4cdb0fac927cacaa116c034f25b7fa7
kingdomik <dnsk@mail.ru>
2014-05-21 22:04:37 +0400

    - Use utf8 for log - Report test start/finish to logger

--- src/ru/odnoklassniki/tests/runner/listeners/TextReporter.java
+++ src/ru/odnoklassniki/tests/runner/listeners/TextReporter.java
@@ -4,26 +4,36 @@ import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 
+import ru.odnoklassniki.tests.common.Loggers;
+
 public class TextReporter extends TestListenerAdapter {
 
 	private String getTestName(ITestResult result) {
 		ITestNGMethod method = result.getMethod();
 		return method.getTestClass().getName() + "." + method.getMethodName();
 	}
+	
+	@Override
+	public void onTestStart(ITestResult result) { 		
+		Loggers.framework.info("====== " + getTestName(result) + " ======");
+	}
 
 	@Override
 	public void onTestFailure(ITestResult result) {
 		System.out.println(" FAILED : " + getTestName(result));
+		Loggers.framework.info("====== " + getTestName(result) + " [FAILED] ======");
 	}
 
 	@Override
 	public void onTestSkipped(ITestResult result) {
 		System.out.println("SKIPPED : " + getTestName(result));
+		Loggers.framework.info("====== " + getTestName(result) + " [SKIPPED] ======");
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
 		System.out.println(" PASSED : " + getTestName(result));
+		Loggers.framework.info("====== " + getTestName(result) + " [PASSED] ======");
 	}
 
 }
